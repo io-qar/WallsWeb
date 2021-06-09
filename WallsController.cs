906cbd81e63fb61d5353b12886ef9e638000f6ba
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-09 15:56:42 +0300

    Add files via upload

---
+++ WallsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WallsWeb.Models;
+
+namespace WallsWeb.Controllers
+{
+    public class WallsController : Controller
+    {
+        // GET: Walls
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Wall(int id)
+        {
+            WallManager wallManager = new WallManager();
+            var wall = wallManager.GetWall(1);
+            return View(wall);
+        }
+    }
+}
