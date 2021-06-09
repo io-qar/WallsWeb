7a9c82280f47e385198d05a362541f2614a31ebf
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-09 15:57:15 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WallsWeb.Models;
+
+namespace WallsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //model
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Comment comment = new Comment();
+            comment.Text = "sdfdsfds";
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
