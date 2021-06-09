66b76ac1fa51a7dce51f9e89d62d2ce9958fbb22
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-09 15:59:43 +0300

    Add files via upload

---
+++ Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WallsWeb.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }//read and write
+        public string Text { get; set; }//read and write
+        public int Rating { get; set; }
+    }
+}
